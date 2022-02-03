10d522da8be7f070859dd3e58733c5d8f16a6afa
Monika Zarzycka <monikajzarzycka@gmail.com>
2022-02-03 23:29:03 +0100

    Commit Animal task

---
+++ TaskThree/src/main/java/mainOOP/Animal.java
@@ -0,0 +1,13 @@
+package mainOOP;
+
+public class Animal {
+
+    protected String name;
+
+    public Animal(String name){
+
+    }
+
+
+
+}
---
+++ TaskThree/src/main/java/mainOOP/Cat.java
@@ -0,0 +1,15 @@
+package mainOOP;
+
+public class Cat extends Animal{
+
+    public Cat(String name) {
+        super(name);
+    }
+}
+
+class AnimalMain{
+    public static void main(String[] args) {
+        Cat cat = new Cat("Mushu");
+        System.out.println(cat.name);
+    }
+}
