128730146b9220926b5405fcf21fbc2a308080ab
Monika Zarzycka <monikajzarzycka@gmail.com>
2022-02-05 18:53:04 +0100

    Commit Triangle Task Final

--- TaskThree/src/main/java/KlasaTriangle/Triangle.java
+++ TaskThree/src/main/java/KlasaTriangle/Triangle.java
@@ -1,8 +1,15 @@
 package KlasaTriangle;
 
 public class Triangle {
+    private int weight;
+    private int height;
 
-    public String toString(int weight, int height) {
+    public Triangle(int weight, int height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public String toString() {
         return "Triangle{" + "weight=" + weight + ", height=" + height + '}';
     }
 }
--- TaskThree/src/main/java/KlasaTriangle/TriangleMain.java
+++ TaskThree/src/main/java/KlasaTriangle/TriangleMain.java
@@ -3,7 +3,7 @@ package KlasaTriangle;
 public class TriangleMain {
 
     public static void main(String[] args) {
-        Triangle triangle = new Triangle();
-        System.out.println(triangle.toString(5, 8));
+        Triangle triangle = new Triangle(5, 8);
+        System.out.println(triangle);
     }
 }
