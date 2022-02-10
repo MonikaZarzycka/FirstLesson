a8237e2aa449630e961f19dfa3970e8fa38449fb
Monika Zarzycka <monikajzarzycka@gmail.com>
2022-02-10 16:24:49 +0100

    Commit Names task

---
+++ TaskThree/src/main/java/JavaCollections/ListForNames.java
@@ -0,0 +1,35 @@
+package JavaCollections;
+
+import java.util.*;
+
+public class ListForNames {
+
+
+        public static void printWithoutDuplicates(List<String> list) {
+            Set<String> set = new LinkedHashSet<>(list);
+            List<String> listWithoutDuplicates = new ArrayList<>(set);
+
+            System.out.println(listWithoutDuplicates);
+        }
+
+        public static void printReversed(List<String> list) {
+            List<String> reversedList = new ArrayList<>(list);
+            Collections.reverse(reversedList);
+
+            System.out.println(reversedList);
+        }
+
+        public static void printWithReplacedElements(String toReplace, String replacement, List<String> list) {
+            List<String> listWithReplacedElements = new ArrayList<>(list);
+            ListIterator<String> iterator = listWithReplacedElements.listIterator();
+            while (iterator.hasNext()) {
+                String next = iterator.next();
+                if(next.equals(toReplace)) {
+                    iterator.set(replacement);
+                }
+            }
+
+            System.out.println(listWithReplacedElements);
+        }
+
+}
